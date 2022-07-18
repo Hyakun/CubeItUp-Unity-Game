ae2b8a264f4bee750cb02f12b0f8804fba5603cd
Hyakun <xdanyro1@gmail.com>
2022-07-18 22:15:18 +0300

    Add  Project

---
+++ CubeItUp/Assets/Boss_Run.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Run : StateMachineBehaviour
+{
+    private float speed = 2f;
+    [SerializeField]
+    private float attackRange = 3f;
+    
+    Transform player;
+    Rigidbody2D rb;
+    Boss boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        { 
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+       rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+        if (player != null)
+        {
+            Vector2 target = new Vector2(player.position.x, rb.position.y);
+        
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+        
+        if (Vector2.Distance(player.position, rb.position)<= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack");
+    }
+
+
+}
